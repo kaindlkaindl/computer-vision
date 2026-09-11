7cda0102766ea344ca82c95e88c24d341b483e6d
kaindlkaindl <gabriel.kaindl.kaindl@hva.nl>
2026-09-11 14:45:14 +0200

    Add rectangle overlay and save processed image as verwerkte_schurk.jpg

--- opdracht_1.py
+++ opdracht_1.py
@@ -9,7 +9,9 @@ args = parser.parse_args()
 image = cv2.imread(args.image_path)
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.rectangle(gray_image, (175, 200), (400, 280), 0, -1)
 
 cv2.imshow('Grayscale', gray_image)
 cv2.waitKey(0)  
 cv2.destroyAllWindows()
+cv2.imwrite('verwerkte_schurk.jpg', gray_image)
