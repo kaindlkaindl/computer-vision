12803171f516e25cdaec51558c327463ac4d8cdd
kaindlkaindl <gabriel.kaindl.kaindl@hva.nl>
2026-09-16 16:00:28 +0200

    Refactor image processing to use anonymization function and update output image

--- opdracht_1.py
+++ opdracht_1.py
@@ -1,6 +1,14 @@
 import argparse
 import cv2
 import numpy as np
+import math
+
+def anonymize(img, eye1, eye2):
+    distance = (math.dist(eye1, eye2))
+    thickness = int(distance * 0.35)
+    result = img.copy()
+    cv2.line(result, eye1, eye2, (0, 0, 0), thickness)
+    return result
 
 parser = argparse.ArgumentParser(description='Verwerk een foto van de schurk')
 parser.add_argument('image_path', type=str, help='Pad naar de afbeelding van de schurk')
@@ -9,9 +17,13 @@ args = parser.parse_args()
 image = cv2.imread(args.image_path)
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.rectangle(gray_image, (175, 200), (400, 280), 0, -1)
 
-cv2.imshow('Grayscale', gray_image)
+eye1 = (200, 250)
+eye2 = (360, 230)
+
+result = anonymize(gray_image, eye1, eye2)
+
+cv2.imshow('Grayscale', result)
 cv2.waitKey(0)  
 cv2.destroyAllWindows()
-cv2.imwrite('verwerkte_schurk.jpg', gray_image)
+cv2.imwrite('verwerkte_schurk.jpg', result)
